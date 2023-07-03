f8bcdadb22b00083b8fc355a6a90c715b37a3630
Gromovoi Stanislav <gromila12@rambler.ru>
2023-07-03 12:47:25 +0600

    graphql-demo 6

---
+++ graphql-demo 6/helloworld/index_1.js
@@ -0,0 +1,51 @@
+import {graphql, buildSchema} from "graphql";
+
+const schema = buildSchema(`
+  type Query {
+    message: String
+    hello: String
+  }
+`);
+
+const resolvers = () => {
+    const message = () => {
+        return "Hello World";
+    };
+    const hello = () => {
+        return "World";
+    };
+
+    return {
+        message,
+        hello
+    };
+};
+
+// execute the query:
+
+// graphql({
+//     schema,
+//     rootValue: resolvers(),
+//     source: "{message}",
+// }).then((response) => {
+//     console.log(response);
+// });
+
+const executeQuery = async () => {
+    let result = await graphql({schema, rootValue: resolvers(), source: "{message}"})
+    console.log(result)
+    // Prints
+    // {
+    //   data: { message: "Hello World" }
+    // }
+
+    result = await graphql({schema, rootValue: resolvers(), source: "{hello}"})
+    console.log(result)
+    // Prints
+    // {
+    //   data: { hello: 'World' }
+    // }
+}
+
+executeQuery()
+
---
+++ graphql-demo 6/helloworld/index_2.js
@@ -0,0 +1,55 @@
+import {
+    graphql,
+    GraphQLSchema,
+    GraphQLObjectType,
+    GraphQLString,
+} from 'graphql';
+
+
+const schema = new GraphQLSchema({
+    query: new GraphQLObjectType({
+        name: 'RootQueryType',
+        fields: {
+            hello: {
+                type: GraphQLString,
+                resolve() {
+                    return 'World';
+                },
+            },
+            helloWorld: {
+                type: GraphQLString,
+                resolve() {
+                    return 'Hello World';
+                },
+            },
+        },
+    }),
+});
+
+
+// execute the query:
+
+// const source = '{ hello }';
+// graphql({ schema, source }).then((result) => {
+//     console.log(result);
+// });
+
+const executeQuery = async () => {
+    let result = await graphql({schema, source:'{ hello }'})
+    console.log(result)
+    // Prints
+    // {
+    //   data: { hello: "World" }
+    // }
+
+    result = await graphql({schema, source:'{ helloWorld }'})
+    console.log(result)
+    // Prints
+    // {
+    //   data: { helloWorld: 'Hello World' }
+    // }
+}
+
+executeQuery()
+
+
